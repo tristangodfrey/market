6c71e926227c40e6fcddb1d933332a8d7e2a3652
Tristan Godfrey <tristan@godfrey.nl>
2021-11-18 17:29:14 +0000

    init

---
+++ build/Generated/Types.hs
@@ -0,0 +1,38 @@
+-- This file is auto generated and will be overriden regulary. Please edit `Application/Schema.sql` to change the Types
+{-# LANGUAGE TypeSynonymInstances, FlexibleInstances, InstanceSigs, MultiParamTypeClasses, TypeFamilies, DataKinds, TypeOperators, UndecidableInstances, ConstraintKinds, StandaloneDeriving  #-}
+{-# OPTIONS_GHC -Wno-unused-imports -Wno-dodgy-imports -Wno-unused-matches #-}
+module Generated.Types where
+
+import IHP.HaskellSupport
+import IHP.ModelSupport
+import CorePrelude hiding (id)
+import Data.Time.Clock
+import Data.Time.LocalTime
+import qualified Data.Time.Calendar
+import qualified Data.List as List
+import qualified Data.ByteString as ByteString 
+import qualified Net.IP 
+import Database.PostgreSQL.Simple
+import Database.PostgreSQL.Simple.FromRow
+import Database.PostgreSQL.Simple.FromField hiding (Field, name)
+import Database.PostgreSQL.Simple.ToField hiding (Field)
+import qualified IHP.Controller.Param
+import GHC.TypeLits
+import Data.UUID (UUID)
+import Data.Default
+import qualified IHP.QueryBuilder as QueryBuilder
+import qualified Data.Proxy
+import GHC.Records
+import Data.Data
+import qualified Data.String.Conversions
+import qualified Data.Text.Encoding
+import qualified Data.Aeson
+import Database.PostgreSQL.Simple.Types (Query (Query), Binary ( .. ))
+import qualified Database.PostgreSQL.Simple.Types
+import IHP.Job.Types
+import IHP.Job.Queue ()
+import qualified Data.Dynamic
+import Data.Scientific
+
+
+
